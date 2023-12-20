90aa556adae9d93f01baa352b794513c01499c67
Vasyl Turok <turokvasil0@gmail.com>
2023-12-20 20:05:54 +0200

    add filter in book

--- books_service/views.py
+++ books_service/views.py
@@ -1,5 +1,5 @@
 from rest_framework import viewsets
-
+from drf_spectacular.utils import OpenApiParameter, extend_schema
 from books_service.models import Book
 from books_service.serializers import BookSerializer
 
@@ -7,3 +7,32 @@ from books_service.serializers import BookSerializer
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+    def get_queryset(self):
+        title = self.request.query_params.get("title")
+        author = self.request.query_params.get("author")
+        queryset = self.queryset
+
+        if title:
+            queryset = queryset.filter(title__icontains=title)
+        if author:
+            queryset = queryset.filter(author__icontains=author)
+
+        return queryset
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                "title",
+                type=str,
+                description="Filter by title. Example: ?title=Some title"
+            ),
+            OpenApiParameter(
+                "author",
+                type=str,
+                description="Filter by author. Example: ?author=Some author"
+            )
+        ]
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
--- library_service/urls.py
+++ library_service/urls.py
@@ -26,5 +26,5 @@ urlpatterns = [
         SpectacularSwaggerView.as_view(url_name="schema"),
         name="swagger-ui"
     ),
-    path("api/doc/books_service/", include("books_service.urls", namespace="book"))
+    path("api/books_service/", include("books_service.urls", namespace="book"))
 ]
