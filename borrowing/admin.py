2d237d9d41f0c4a1a9640461e99050ae6125de9d
Vasyl Turok <turokvasil0@gmail.com>
2023-12-21 19:46:15 +0200

    add borrowing model

--- borrowing/admin.py
+++ borrowing/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from borrowing.models import Borrowing
 
-# Register your models here.
+
+admin.site.register(Borrowing)
--- borrowing/models.py
+++ borrowing/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+from django.conf import settings
+from books_service.models import Book
+
+
+class Borrowing(models.Model):
+    borrow_date = models.DateField(auto_now_add=True)
+    expected_return_date = models.DateField()
+    actual_return_date = models.DateField(null=True, blank=True)
+    book = models.ForeignKey(
+        Book,
+        related_name="borrowing",
+        on_delete=models.CASCADE
+    )
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return f"{self.borrow_date} - {self.expected_return_date}"
+
+    class Meta:
+        ordering = ["borrow_date", ]
