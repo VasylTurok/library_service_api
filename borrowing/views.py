bd83c27565fd2b3bb9382e1031c750cbea184b3b
Vasyl Turok <turokvasil0@gmail.com>
2023-12-22 19:19:32 +0200

    add is_active annotate & filter

--- borrowing/views.py
+++ borrowing/views.py
@@ -1,30 +1,76 @@
-from rest_framework import viewsets
+from django.db.models import F, Count, Case, When, Value, BooleanField
+from drf_spectacular.utils import extend_schema, OpenApiParameter
+from rest_framework import viewsets, mixins
 from rest_framework.decorators import api_view
 from rest_framework.request import Request
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import GenericViewSet
 
 from borrowing.models import Borrowing
 from borrowing.serializers import (
     BorrowingSerializer,
-    BorrowingListOrRetrieveSerializer
+    BorrowingListSerializer
 )
 
 
-class BorrowingViewSet(viewsets.ModelViewSet):
-    queryset = Borrowing.objects.select_related("user", "book")
-    serializer_class = BorrowingSerializer
+class BorrowingViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet
+):
+    queryset = Borrowing.objects.select_related("user", "book").annotate(
+        is_active=Case(
 
-    # def get_queryset(self):
-    #     queryset = Borrowing.objects.select_related("user", "book")
-    #     if not self.request.user.is_staff:
-    #         queryset = queryset.filter(user_id=self.request.user.id)
-    #     return queryset
+            When(actual_return_date__isnull=True, then=Value(True)),
+            default=Value(False),
+            output_field=BooleanField(),
+        )
+    )
+    serializer_class = BorrowingSerializer
+    permission_classes = (IsAuthenticated, )
+
+    def get_queryset(self):
+        queryset = self.queryset
+        if not self.request.user.is_staff:
+            queryset = queryset.filter(user_id=self.request.user.id)
+
+        is_active = self.request.query_params.get("is_active")
+        if is_active:
+            if is_active == "False":
+                queryset = queryset.filter(actual_return_date__isnull=False)
+            else:
+                queryset = queryset.filter(actual_return_date__isnull=True)
+
+        return queryset
 
     def get_serializer_class(self):
-        if self.action == "list" or self.action == "retrieve":
-            return BorrowingListOrRetrieveSerializer
+        if self.action == "list":
+            return BorrowingListSerializer
         return self.serializer_class
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                "is_active",
+                type=bool,
+                description="Filter by is_active. "
+                            "Use True or False."
+                            "Example: ?is_active=True"
+            ),
+            OpenApiParameter(
+                "arrival_time",
+                type=str,
+                description="Filter by arrival_time. "
+                            "Example: ?arrival_time=2000-12-1"
+            )
+        ]
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
+
 
 #
 # @api_view(["POST"])
-# def return_book(request: Request) -> Response:
+# def return_book(request: Request) -> Response: ? що відправляти
